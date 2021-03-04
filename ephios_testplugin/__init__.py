445518b6800e5b868f30035d795b85df7909e2aa
Felix Rindt <felix@rindt.me>
2021-03-04 18:45:48 +0100

    rename entrypoint

--- ephios_testplugin/__init__.py
+++ ephios_testplugin/__init__.py
@@ -1,19 +1,19 @@
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy
 
-from ephios.plugins import PluginConfig
+from ephios.core.plugins import PluginConfig
+
 
 class PluginApp(PluginConfig):
-    name = 'ephios_testplugin'
-    verbose_name = 'Ephios test plugin'
+    name = "ephios_testplugin"
 
     class EphiosPluginMeta:
         name = "Ephios Test Plugin"
-        author = 'Felix Rindt'
+        author = "Felix Rindt"
         description = "A test plugin for ephios"
 
     def ready(self):
         from . import signals  # NOQA
 
 
-default_app_config = 'ephios_testplugin.PluginApp'
+default_app_config = "ephios_testplugin.PluginApp"
--- ephios_testplugin/signals.py
+++ ephios_testplugin/signals.py
@@ -1,7 +1,10 @@
 from django.dispatch import receiver
 
-from ephios.extra.signals import footer_link
+from ephios.core.signals import footer_link
 
-@receiver(footer_link, dispatch_uid="ephios.plugins.test_plugin.signals.pages_footer_links")
+
+@receiver(
+    footer_link, dispatch_uid="ephios.plugins.test_plugin.signals.pages_footer_links"
+)
 def pages_footer_links(sender, request, **kwargs):
     return {"Test Plugin is here": "https://example.com"}
